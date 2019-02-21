0a2abfce49bbd48925f81a34f2125de2b70916d7
Tomasz Kapłon <tomasz.kaplon@monterail.com>
2019-02-21 19:04:15 +0100

    first version

--- generate.rb
+++ generate.rb
@@ -7,36 +7,30 @@ TRANSLATION_ID = 3 # 2 - IV wydanie, 3 - V wydanie
 
 books = db.execute "SELECT id, number, shortTitle FROM book WHERE translation_id = #{TRANSLATION_ID} ORDER BY number ASC;"
 
-# File.open("ksiegi.biblia", "a") do |f|
-#   books.each do |book|
-#     _id, number, shortTitle = book
-#     f.puts "#{number} #{shortTitle}"
-#   end
-# end
+File.open("ksiegi.biblia", "a") do |f|
+  books.each do |book|
+    _id, number, shortTitle = book
+    f.puts "#{number} #{shortTitle}"
+  end
+end
 
 File.open("bt5.biblia", "a") do |f|
-  [books[0]].each do |book|
-    book_id, book_number, _shortTitle = book
+  books.each do |book|
+    book_id, book_number, shortTitle = book
 
-    chapters = db.execute "SELECT number, content, verseCount FROM chapter WHERE book_id = #{book_id} ORDER BY number ASC;"
+    chapters = db.execute "SELECT number, content FROM chapter WHERE book_id = #{book_id} ORDER BY number ASC;"
 
-    [chapters[0]].each do |chapter|
-      chapter_number, content, verse_count = chapter
+    chapters.each do |chapter|
+      chapter_number, content = chapter
 
       html = Nokogiri::HTML(content)
       verses = html.css('.verse')
 
-      raise "Invalid verses count" if verses.size != verse_count
-
-      [verses[0]].each do |verse|
+      verses.each_with_index do |verse, i|
+        verse_number = i + 1
         verse_text = verse.text.strip
+        f.puts "#{ book_number.to_s.rjust(3, "0") }#{ chapter_number.to_s.rjust(3, "0") }#{ verse_number.to_s.rjust(3, "0") } #{verse_text}"
       end
     end
   end
 end
-#
-# chapter_html = Nokogiri::HTML("")
-#
-# chapter_html.css('.verse')
-#
-# pp books
